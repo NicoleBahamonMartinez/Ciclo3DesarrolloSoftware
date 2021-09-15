b5390ef7a9b944f400a9982a5a7048e0e7a05bcc
Nicole Bahamón Martínez <47301184+NicoleBahamonMartinez@users.noreply.github.com>
2021-09-15 08:54:12 -0500

    Creacion Entidad Visita Domiciliaria

---
+++ MascotaFeliz/MascotaFeliz.App/MascotaFeliz.App.Dominio/Entidades/VisitaDomiciliario.cs
@@ -0,0 +1,14 @@
+using System;
+namespace MascotaFeliz.App.Dominio
+{
+    public class VisitaDomiciliaria
+    {
+        public string Placa {get;set;}
+        public string TipoVehiculo {get;set;}
+        public string Marca {get;set;}
+        public string Modelo {get;set;}
+        public string color {get;set;}
+        public Puesto ubicacion {get:set:}
+
+    }
+}
