3e2f584b12ed28203d8b1f124fb7604e3ac269b9
EdissonC <90624274+EdissonC@users.noreply.github.com>
2021-09-15 09:00:21 -0500

    Entidad mascota

---
+++ MascotaFeliz/MascotaFeliz.App/MascotaFeliz.App.Dominio/Entidades/Mascota.cs
@@ -0,0 +1,15 @@
+using System;
+namespace MascotaFeliz.App.Dominio
+{
+    public class Mascota
+    {
+        public int id {get;set;}
+        public string Nombre {get;set;}
+        public int Edad {get;set;}
+        public string Tipo {get;set;}
+        public string Raza {get;set;}
+        public string Color {get;set;}
+        
+
+    }
+}
