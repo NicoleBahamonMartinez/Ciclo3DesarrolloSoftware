574c67538c54b3f4e40b32f7d7391bae788dc81c
OlgaLuciaDiaz <85802783+OlgaLuciaDiaz@users.noreply.github.com>
2021-09-15 08:58:11 -0500

    Update Persona.cs

---
+++ MascotaFeliz/MascotaFeliz.App/MascotaFeliz.App.Dominio/Entidades/Persona.cs
@@ -0,0 +1,15 @@
+using System;
+namespace MascotaFeliz.App.Dominio
+{
+    public class Usuario
+    {
+        public string TipoIdentificacion {get;set;}
+  public int NroIdentificacion {get;set;}
+        public string Nombres {get;set;}
+        public string Apellidos {get;set;}
+        public long NumeroContacto {get;set;}
+        public string CorreoElectronico {get;set;}
+
+    }
+}
+
